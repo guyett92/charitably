0ee10de8abdc6cfa252a0f5707942bc89823261c
guyett92 <aaronguyett@yahoo.com>
2020-09-08 22:07:56 -0700

    adds base

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/App/index';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+      <Route component={App} />
+  </Router>,
   document.getElementById('root')
 );
 
---
+++ src/pages/App/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>React</h1>
+            <p>Test</p>
+        </div>
+    )
+};
+  
+export default App;
